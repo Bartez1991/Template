5ee26bd177c0290749c1a441ea89a6b382bac805
Bartez1991 <zapisotskiy.igor@gmail.com>
2019-12-06 00:50:08 +0200

    add styles files

--- wp-content/themes/be_theme/gulpfile.js
+++ wp-content/themes/be_theme/gulpfile.js
@@ -27,16 +27,28 @@ gulp.task('theme-css', function(){
   return gulp
       .src(pathCSS + 'theme/main.scss')
       .pipe(sourcemaps.init())
-      .pipe(sass({outputStyle: 'compressed'}).on('error',  notify.onError(notifyOptions)))
-      .pipe(autoprefixer({ browsers: ['last 99 versions'], cascade: false }))
+      .pipe(sass({outputStyle: 'expanded'}).on('error',  notify.onError(notifyOptions)))
+      .pipe(autoprefixer({ overrideBrowserslist: ['last 99 versions'], cascade: false }))
       .pipe(concat('main.min.css'))
       .pipe(sourcemaps.write('.'))
       .pipe(gulp.dest(pathCSS))
       .pipe(livereload());
 });
 /* Compile Vendor Styles */
+gulp.task('vendor-css', function(){
+  return gulp
+      .src(pathCSS + 'vendor/vendor.scss')
+      .pipe(sourcemaps.init())
+      .pipe(sass({outputStyle: 'compressed'}).on('error',  notify.onError(notifyOptions)))
+      .pipe(autoprefixer({ overrideBrowserslist: ['last 99 versions'], cascade: false }))
+      .pipe(concat('vendor.min.css'))
+      .pipe(sourcemaps.write('.'))
+      .pipe(gulp.dest(pathCSS))
+      .pipe(livereload());
+});
+
 /* Compile Theme Scripts */
-gulp.task('theme-scripts', function() {
+gulp.task('theme-js', function() {
   return gulp
       .src(pathJS + 'theme/*.js')
       .pipe(concat('main.min.js'))
@@ -46,6 +58,39 @@ gulp.task('theme-scripts', function() {
       .pipe(gulp.dest(pathJS))
       .pipe(livereload());
 });
+
 /* Compile Vendor Scripts */
+gulp.task('vendor-js', function() {
+  return gulp
+      .src(pathJS + 'vendor/*.js')
+      .pipe(concat('vendor.min.js'))
+      .pipe(sourcemaps.init())
+      .pipe(uglify())
+      .pipe(sourcemaps.write('.'))
+      .pipe(gulp.dest(pathJS))
+      .pipe(livereload());
+});
 
+/* Compile Front CSS for Gutenberg admin */
+gulp.task('admin-css', function(){
+  return gulp
+      .src(pathCSS + 'admin/admin.scss')
+      .pipe(sourcemaps.init())
+      .pipe(sass({outputStyle: 'expanded'}).on('error',  notify.onError(notifyOptions)))
+      .pipe(autoprefixer({ overrideBrowserslist: ['last 99 versions'], cascade: false }))
+      .pipe(concat('admin.min.css'))
+      .pipe(sourcemaps.write('.'))
+      .pipe(gulp.dest(pathCSS))
+      .pipe(livereload());
+});
 
+/* Run task for continuous track of css & js files */
+gulp.task('all', function(){
+  livereload.listen();
+  // theme
+  gulp.watch(pathCSS + 'theme/**/*.scss',  gulp.series('theme-css','admin-css'));
+  gulp.watch(pathJS + 'theme/*.js',  gulp.series('theme-js'));
+  // vendor
+  gulp.watch(pathCSS + 'vendor/**/*.scss',  gulp.series('vendor-css'));
+  gulp.watch(pathJS + 'vendor/*.js',  gulp.series('vendor-js'));
+});
